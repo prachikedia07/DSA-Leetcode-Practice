8c06eaeae806a9e7939864100e290b81370763ca
Prachi Kedia <prachikedia2005@gmail.com>
2025-06-11 23:41:28 +0530

    Create Implement-strStr().cpp

---
+++ Week-2( Strings & Searching)/Implement-strStr().cpp
@@ -0,0 +1,40 @@
+/* NEW TITLE OF THE QUESTION IS: "FIND THE INDEX OF THE FIRST OCCURENCE IN A STRING"
+
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+
+
+A
+
+
+
+   TIME COMPLEXITY-
+   O(n * m)   
+   SPACE COMPLEXITY-
+   O(m)     
+                                                                                                */
+
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n=haystack.size();
+        int m=needle.size();
+      
+        if(m==0){
+            return 0;        //edge case
+        }
+      
+        for(int i=0;i<=n-m;i++){
+            if(haystack.substr(i,m)==needle){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
