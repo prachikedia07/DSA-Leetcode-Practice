46f135f1c37a048debe8f4daff3a6fd384d20109
Prachi Kedia <prachikedia2005@gmail.com>
2025-06-25 00:15:42 +0530

    Create number-of-1-bits.cpp

---
+++ Week-5( Bit Manipulation + Maths)/number-of-1-bits.cpp
@@ -0,0 +1,15 @@
+# Code
+```cpp []
+class Solution {
+public:
+    int hammingWeight(int n) {
+       
+        int count = 0;
+       while(n>0){
+        count += n & 1 ;
+        n >>= 1;
+       }
+        return count;
+    }
+};
+```
