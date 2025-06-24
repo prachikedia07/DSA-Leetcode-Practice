0962858f23beb0b50170c55d0a58c028c0127ed4
Prachi Kedia <prachikedia2005@gmail.com>
2025-06-25 00:45:37 +0530

    Create single-number.cpp

---
+++ Week-5( Bit Manipulation + Maths)/single-number.cpp
@@ -0,0 +1,13 @@
+# Code
+```cpp []
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int x : nums){
+            ans = ans^x;
+        }
+        return ans;
+    }
+};
+```
