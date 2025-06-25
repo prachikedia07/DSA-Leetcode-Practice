a5f680d32a2594da7e81ba48b10bfd28812cd579
Prachi Kedia <prachikedia2005@gmail.com>
2025-06-25 20:17:25 +0530

    Create excel-sheet-column-number.cpp

---
+++ Week-5( Bit Manipulation + Maths)/excel-sheet-column-number.cpp
@@ -0,0 +1,13 @@
+# Code
+```cpp []
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int res = 0;
+        for(char c : columnTitle){
+            res = res*26 + ( c - 'A' + 1);
+        }
+        return res;
+    }
+};
+```
