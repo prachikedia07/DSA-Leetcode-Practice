ffa5bd659179db0b7883978c7ebbf9969776e5ba
Prachi Kedia <prachikedia2005@gmail.com>
2025-07-04 23:24:35 +0530

    Create symmetric-tree.cpp

---
+++ Week-8( BST + Trees )/symmetric-tree.cpp
@@ -0,0 +1,27 @@
+# Code
+```cpp []
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+bool isMirror(TreeNode* left, TreeNode* right){
+if(!left && !right) return true;
+        if(!left || !right) return false;
+        bool final = ((left->val == right->val) && isMirror(left->left, right->right) && isMirror(left->right, right->left));
+        return final;
+}
+    bool isSymmetric(TreeNode* root) {
+         if(root == NULL) return NULL;
+        return isMirror(root->left, root->right);  
+    }
+};
+```
